70e111342725b7690b0c092a63fbe5c449e26efc
powdergotpower <anshjotsinghsidhu@gmail.com>
2025-09-09 12:53:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ BOT_TOKEN = "8392213332:AAE8vq4X1GbmuOmmX6Hdix7CUwTvAtb3iQ0"
 
 # Channel settings
 CHANNEL_LINK = "https://t.me/salaryget"   # Full link for the Join Channel button
+CHANNEL_USERNAME = "@salaryget"           # Use @username for membership check
 
 # Bot settings
 REFERRAL_REWARD = 2.5    # Reward per referral
