c4d199e8255b7227a62c49ac07f974b2f02cf106
powdergotpower <anshjotsinghsidhu@gmail.com>
2025-09-09 12:22:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 BOT_TOKEN = "8392213332:AAE8vq4X1GbmuOmmX6Hdix7CUwTvAtb3iQ0"
-CHANNEL_USERNAME = -1003092866421   # note: integer, no quotes
+
+# Channel settings
+CHANNEL_LINK = "https://t.me/salaryget"   # for button URL
+
+# Bot settings
 REFERRAL_REWARD = 2.5
 MIN_WITHDRAW = 100
 DATA_FILE = "data.json"
