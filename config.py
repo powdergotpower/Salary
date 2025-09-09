f51acc3592de416904e08b887d6155f38539c7c4
powdergotpower <anshjotsinghsidhu@gmail.com>
2025-09-09 12:41:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
+# Bot token
 BOT_TOKEN = "8392213332:AAE8vq4X1GbmuOmmX6Hdix7CUwTvAtb3iQ0"
 
 # Channel settings
-CHANNEL_LINK = "https://t.me/salaryget"   # for button URL
+CHANNEL_LINK = "https://t.me/salaryget"   # Full link for the Join Channel button
 
 # Bot settings
-REFERRAL_REWARD = 2.5
-MIN_WITHDRAW = 100
-DATA_FILE = "data.json"
+REFERRAL_REWARD = 2.5    # Reward per referral
+MIN_WITHDRAW = 100       # Minimum units required to withdraw
+DATA_FILE = "data.json"  # File to store user data
